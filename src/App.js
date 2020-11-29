d39ac51e62d30c764913c0fdb49873a555106b63
Roy <rdg01834@gmail.com>
2020-11-29 22:55:55 +0600

    wrapping token from url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+      const hash = getTokenFromUrl();
+      window.location.hash = "";
+      const _token = hash.access_token;
+
+      if (_token) {
+        setToken(_token) 
+      }
+
+      console.log("i have token", token)
+  },[])
+
   return (
     <div className="App">
+      {
+        token ? 
+          <h1>i am log in</h1>
+         : <Login></Login>
+      }
 
       <Login></Login>
-      
+
     </div>
   );
 }
